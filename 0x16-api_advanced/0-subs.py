9caffbf955b84dc41753c91fb0893f7323827502
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-12 07:23:14 +0300

    formating

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,15 +6,24 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     # Set the custom User-Agent header
     headers = {"User-Agent": "reddit_api/0.0.1"}
+    
     # Make a GET request to the subreddit's about.json endpoint
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers=headers
+    )
+    
     # Check the status code and the data type of the response
-    if response.status_code == 200 and response.headers["content-type"] == "application/json; charset=UTF-8":
+    if (
+        response.status_code == 200 and 
+        response.headers["content-type"] == "application/json; charset=UTF-8"
+    ):
         # Parse the JSON data
         data = response.json()
+        
         # Return the number of subscribers from the data
         return data["data"]["subscribers"]
+    
     else:
         # Return 0 for invalid subreddit
         return 0
-
