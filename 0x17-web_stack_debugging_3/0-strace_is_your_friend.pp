6856060eef8fb836908bbbc5f7f170e5a93e07d1
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-24 15:05:01 +0300

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,7 @@
 # Puppet manifest to fix Apache 500 error
 
-# Define a package resource to ensure the necessary package is installed
-package { 'example-package':
-  ensure => installed,
-}
-
-# Define a file resource to modify an Apache configuration file
-file { '/etc/apache2/apache.conf':
-  ensure  => present,
-  content => template('module/apache.conf.erb'),
-  notify  => Service['apache2'],
-}
-
-# Define a service resource to restart Apache
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/apache2/apache.conf'],
+$file_pth = '/var/www/html/wp-settings.php'
+exec{ 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_pth}",
+  path    => ['/bin','/usr/bin']
 }
