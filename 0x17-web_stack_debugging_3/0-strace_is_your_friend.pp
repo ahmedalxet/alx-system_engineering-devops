38e4cfa133dab559dd566ec9e939ee60f5c6f86b
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-24 14:50:32 +0300

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,37 +1,20 @@
-# 0-strace_is_your_friend.pp
+# Puppet manifest to fix Apache 500 error
 
-# Install strace package
-package { 'strace':
+# Define a package resource to ensure the necessary package is installed
+package { 'example-package':
   ensure => installed,
 }
 
-# Create a bash script to run strace on the Apache process
-file { '/tmp/strace_apache.sh':
+# Define a file resource to modify an Apache configuration file
+file { '/etc/apache2/apache.conf':
   ensure  => present,
-  content => "#!/bin/bash\nstrace -p \$(pgrep apache2) -o /tmp/strace_output.txt",
-  mode    => '0755',
+  content => template('module/apache.conf.erb'),
+  notify  => Service['apache2'],
 }
 
-# Execute the bash script to start capturing strace output
-exec { 'capture_strace_output':
-  command     => '/tmp/strace_apache.sh',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
-}
-
-# Define the resource to fix the issue based on the strace output
-# Replace 'RESOURCE_TYPE' and 'RESOURCE_NAME' with the appropriate resource type and name
-# For example, if the issue is related to a misconfigured Apache module, you might use 'apache::module' resource type
-RESOURCE_TYPE { 'RESOURCE_NAME':
-  ensure => present,  # Replace with the desired state
-  # Add any other necessary attributes to fix the issue
-}
-
-# Notify the resource to execute the fix when the strace output file changes
-notify { 'execute_fix':
-  require  => Exec['capture_strace_output'],
-  subscribe => File['/tmp/strace_output.txt'],
-  # Replace 'RESOURCE_TYPE[RESOURCE_NAME]' with the appropriate resource type and name
-  # For example, if the issue is related to a misconfigured Apache module, you might use 'apache::module[mod_name]'
-  notify   => RESOURCE_TYPE['RESOURCE_NAME'],
+# Define a service resource to restart Apache
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/apache2/apache.conf'],
 }
