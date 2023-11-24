0555f6ea33cc0d652376739676cf1dc19f0dad54
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-24 12:12:46 +0300

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,37 @@
-class web_stack_debugging {
-  package { 'apache2':
-    ensure => installed,
-  }
+# 0-strace_is_your_friend.pp
 
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => '<html><body><h1>It works!</h1></body></html>',
-    require => Package['apache2'],
-  }
-
-  service { 'apache2':
-    ensure  => running,
-    enable  => true,
-    require => File['/var/www/html/index.html'],
-  }
+# Install strace package
+package { 'strace':
+  ensure => installed,
 }
 
-include web_stack_debugging
+# Create a bash script to run strace on the Apache process
+file { '/tmp/strace_apache.sh':
+  ensure  => present,
+  content => "#!/bin/bash\nstrace -p \$(pgrep apache2) -o /tmp/strace_output.txt",
+  mode    => '0755',
+}
+
+# Execute the bash script to start capturing strace output
+exec { 'capture_strace_output':
+  command     => '/tmp/strace_apache.sh',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+}
+
+# Define the resource to fix the issue based on the strace output
+# Replace 'RESOURCE_TYPE' and 'RESOURCE_NAME' with the appropriate resource type and name
+# For example, if the issue is related to a misconfigured Apache module, you might use 'apache::module' resource type
+RESOURCE_TYPE { 'RESOURCE_NAME':
+  ensure => present,  # Replace with the desired state
+  # Add any other necessary attributes to fix the issue
+}
+
+# Notify the resource to execute the fix when the strace output file changes
+notify { 'execute_fix':
+  require  => Exec['capture_strace_output'],
+  subscribe => File['/tmp/strace_output.txt'],
+  # Replace 'RESOURCE_TYPE[RESOURCE_NAME]' with the appropriate resource type and name
+  # For example, if the issue is related to a misconfigured Apache module, you might use 'apache::module[mod_name]'
+  notify   => RESOURCE_TYPE['RESOURCE_NAME'],
+}
